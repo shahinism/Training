3f6822c2b7a7d1afc0b81cd0b61fdc98593d152e
Shahin Azad <ishahinism@gmail.com>
2014-07-19 20:05:42 +0430

    Problem #2 under the belt.

--- 002.py
+++ 002.py
@@ -10,5 +10,18 @@ previous two terms. By starting with 1 and 2, the first 10 terms will be:
 Find the sum of all the even-valued terms in the sequence which do not
 exceed four million.
 """
+def Fibonacci(n):
+    if n < 2:
+        return n
+    return Fibonacci(n - 1) + Fibonacci(n - 2)
 
+sum = 0
+i = 0
+while True:
+    if Fibonacci(i) % 2 != 0:
+        sum += Fibonacci(i)
+        if sum > 4000000:
+            break
+    i += 1
 
+print sum
