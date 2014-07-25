88b33327d7885068f7d5b2ca27c019eb4c8575d0
Shahin Azad <ishahinism@gmail.com>
2014-07-25 11:43:02 +0430

    First version of the result. It's working for numbers like 13159 but not for 600851475143? since it has not enough memmory

--- 003.py
+++ 003.py
@@ -8,3 +8,16 @@ What is the largest prime factor of the number 600851475143?
 """
 
 
+def isPrime(number):
+    for n in range(2, number - 1):
+        if number % n == 0:
+            return False
+    return True
+
+
+def primeFactor(number):
+    for n in range(2, number):
+        if number % n == 0 and isPrime(n):
+            print n
+
+primeFactor(600851475143)
