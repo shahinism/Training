9f084d1be44793ca5d05e48ed75088e33c57e4e0
Shahin Azad <ishahinism@gmail.com>
2014-08-01 19:27:58 +0430

    Problem 4 under the belt.

--- project-euler/004.py
+++ project-euler/004.py
@@ -9,3 +9,18 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 """
 
 
+def is_palindromic(number):
+    """
+    Return True if the number is a palindromic.
+    """
+    return number == int(str(number)[::-1])
+
+largest_palindrome = 0
+
+for val1 in range(100, 999):
+    for val2 in range(val1, 999):
+        product = val1 * val2
+        if is_palindromic(product) and largest_palindrome < product:
+            largest_palindrome = product
+
+print largest_palindrome
