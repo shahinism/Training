676362e3074ea653a5e0c669fcef6c6f311c628a
Shahin Azad <ishahinism@gmail.com>
2014-08-04 23:42:41 +0430

    Solving problem 005 using LCM algorithm

--- project-euler/005.py
+++ project-euler/005.py
@@ -8,20 +8,19 @@ from 1 to 10 without any remainder.
 What is the smallest number that is evenly divisible by all of the numbers
 from 1 to 20?
 """
-number = 232792560 - 10
 
-def divisible(number):
-    for value in range(1, 21):
-        if number % value != 0:
-            return False
-    return True
+# Resource: http://www.s-anand.net/euler.html
+# https://en.wikipedia.org/wiki/Greatest_common_divisor
+def gcd(a, b):
+    return b and gcd(b, a % b) or a
 
 
-while True:
-    if divisible(number):
-        print number
-        break
-    else:
-        print "Test failed: " + str(number)
-        number += 1
-            
+# https://en.wikipedia.org/wiki/Least_common_multiple#A_simple_algorithm
+def lcm(a, b):
+    return abs(a * b) / gcd(a, b)
+
+n = 1
+for i in xrange(1, 21):
+    n = lcm(n, i)
+
+print n
