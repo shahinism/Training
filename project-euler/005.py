e4da0304e8cef94805334f0071cb6256e2ed1318
Shahin Azad <ishahinism@gmail.com>
2014-08-04 23:30:16 +0430

    First answer to the 005 question. It takes a lot to calculate the right answer

--- project-euler/005.py
+++ project-euler/005.py
@@ -8,5 +8,20 @@ from 1 to 10 without any remainder.
 What is the smallest number that is evenly divisible by all of the numbers
 from 1 to 20?
 """
+number = 232792560 - 10
+
+def divisible(number):
+    for value in range(1, 21):
+        if number % value != 0:
+            return False
+    return True
 
 
+while True:
+    if divisible(number):
+        print number
+        break
+    else:
+        print "Test failed: " + str(number)
+        number += 1
+            
