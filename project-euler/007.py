f6ed9e45e0e6e5e8d925c11b5dd024c8959d0507
Shahin Azad <ishahinism@gmail.com>
2014-08-17 19:51:55 +0430

    Problem 7 under the belt. my way.

--- project-euler/007.py
+++ project-euler/007.py
@@ -7,5 +7,28 @@ that the 6^th prime is 13.
 
 What is the 10001^st prime number?
 """
+import math
 
 
+def is_prime(number):
+    if number % 2 == 0:
+        return False
+    for value in range(2, int(math.ceil(math.sqrt(number)))):
+        if number % value == 0:
+            return False
+    return True
+
+
+def nth_prime(n):
+    count = 0
+    number = 2
+    while True:
+        if is_prime(number):
+            count += 1
+        if count == n:
+            break
+        number += 1
+    return number
+
+
+print(nth_prime(10001))
