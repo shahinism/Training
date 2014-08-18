b24d747d95d66ce92356b4811daef971e9f0aaff
Shahin Azad <ishahinism@gmail.com>
2014-08-18 17:17:35 +0430

    Problem 007 with my solution had a problem. It was returning a wrong answer. I found a way to solve this problem again with correct result.

--- project-euler/007.py
+++ project-euler/007.py
@@ -7,14 +7,25 @@ that the 6^th prime is 13.
 
 What is the 10001^st prime number?
 """
-import math
 
 
-def is_prime(number):
-    if number % 2 == 0:
+def isprime(n):
+    '''check ifinteger n is a prime'''
+    # make sure n is a positive integer
+    n = abs(int(n))
+    # 0 and 1 are not primes
+    if n < 2 and not n & 1:
         return False
-    for value in range(2, int(math.ceil(math.sqrt(number)))):
-        if number % value == 0:
+    # 2 is the only even prime number
+    if n == 2:
+        return True
+    # all other even numbers are not primes
+    if not n & 1:
+        return False
+    # range starts with 3 and only needs to go up the squareroot of n
+    # for all odd numbers
+    for x in range(3, int(n**0.5)+1, 2):
+        if n % x == 0:
             return False
     return True
 
@@ -23,7 +34,7 @@ def nth_prime(n):
     count = 0
     number = 2
     while True:
-        if is_prime(number):
+        if isprime(number):
             count += 1
         if count == n:
             break
